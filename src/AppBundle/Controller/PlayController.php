5a869024a51482c7739b9d2e446b9885f6b61f1c
Jim Schuman <jim.schuman@gmail>
2015-08-26 16:50:00 -0500

    Moving stuff on the game screen
    Moving stuff on the game screen to make it a little better

--- src/AppBundle/Controller/PlayController.php
+++ src/AppBundle/Controller/PlayController.php
@@ -30,7 +30,7 @@ class PlayController extends Controller
 
         return $this->render('default/play.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),'form' => $form->createView(),
-            'user_choice' => $user_choice, 'sid' => $sessionID,
+            'user_choice' => $user_choice
         ));
     }
 
@@ -39,10 +39,7 @@ class PlayController extends Controller
     **/
     public function gameAction(Request $request)
     {    
-        $outcome = "";
-        $sessionID = $this->getRequest()->getSession()->getId();
-        $user_choice = 0;
-        $comp_choice = 0;
+
         $match = new RpsslMatch();
 
         $form = $this->createFormBuilder( $match )
@@ -56,227 +53,279 @@ class PlayController extends Controller
             ->getForm();    
         $form->handleRequest($request);
 
-        /**
-            * 1 = rock
-            * 2 = paper
-            * 3 = scissors
-            * 4 = lizard
-            * 5 = spock
-            *
-            * SCISSORS CUTS PAPER
-            * PAPER COVERS ROCK
-            * ROCK CRUSHES LIZARD
-            * LIZARD POISONS SPOCK
-            * SPOCK SMASHES SCISSORS
-            * SCISSORS DECAPITES LIZARD
-            * LIZARD EATS PAPER
-            * PAPER DISPROVES SPOCK
-            * SPOCK VAPORIZES ROCK
-            * ROCK CRUSHES SCISSORS
-        **/
+        if(
+            $form->get('user_rock')->isClicked() ||
+            $form->get('user_paper')->isClicked() ||
+            $form->get('user_scissors')->isClicked() ||
+            $form->get('user_lizard')->isClicked() ||
+            $form->get('user_spock')->isClicked()
 
-        $comp_choice = rand( 1, 5 );
-        if( $comp_choice == 1 ){
-            $match->setCompRock( 1 );
-            $match->setCompPaper( 0 );
-            $match->setCompScissors( 0 );
-            $match->setCompLizard( 0 );
-            $match->setCompSpock( 0 );
-        }
-        if( $comp_choice == 2){
-            $match->setCompRock( 0 );
-            $match->setCompPaper( 1 );
-            $match->setCompScissors( 0 );
-            $match->setCompLizard( 0 );
-            $match->setCompSpock( 0 );
-        }
-        if( $comp_choice == 3){
-            $match->setCompRock( 0 );
-            $match->setCompPaper( 0 );
-            $match->setCompScissors( 1 );
-            $match->setCompLizard( 0 );
-            $match->setCompSpock( 0 );
-        }
-        if( $comp_choice == 4){
-            $match->setCompRock( 0 );
-            $match->setCompPaper( 0 );
-            $match->setCompScissors( 0 );
-            $match->setCompLizard( 1 );
-            $match->setCompSpock( 0 );
-        }
-        if( $comp_choice == 5){
-            $match->setCompRock( 0 );
-            $match->setCompPaper( 0 );
-            $match->setCompScissors( 0 );
-            $match->setCompLizard( 0 );
-            $match->setCompSpock( 1 );
-        }
-
-        $match->setUserSid( $sessionID );
-
-        if ( $form->get('user_rock')->isClicked() ) {
-            $user_choice = 1;
-            $match->setUserRock( 1 );
-            $match->setUserPaper( 0 );
-            $match->setUserScissors( 0 );
-            $match->setUserLizard( 0 );
-            $match->setUserSpock( 0 );
-            if( $user_choice == $comp_choice ){
-                $outcome = "TIE";
-                $match->setCompRock( 1 );
-                $match->setUserCompTie( true );
-                $match->setUserWon( false );
-                $match->setCompWon( false );
-            }
-            else {
-                $match->setUserCompTie( false );
-                if( $comp_choice == 2 || $comp_choice == 5 ) {
-                    $outcome = "YOU LOSE";
-                    $match->setCompWon( true );
-                    $match->setUserWon( false );
-                }
-                
-                else {
-                    $outcome = "YOU WIN";
-                    $match->setUserWon( true );
-                    $match->setCompWon( false );
-                }
-            }
-            
-        }
-        if ( $form->get('user_paper')->isClicked() ) {
-            $user_choice = 2;
-            $match->setUserRock( 0 );
-            $match->setUserPaper( 1 );
-            $match->setUserScissors( 0 );
-            $match->setUserLizard( 0 );
-            $match->setUserSpock( 0 );
-            if( $user_choice == $comp_choice ){
-                $outcome = "TIE";
-                $match->setCompPaper( 1 );
-                $match->setUserCompTie( true );
-                $match->setUserWon( false );
-                $match->setCompWon( false );
-            }
-            else {
-                $match->setUserCompTie( false );
-                if( $comp_choice == 3 || $comp_choice == 4 ) {
-                    $outcome = "YOU LOSE";
-                    $match->setCompWon( true );
-                    $match->setUserWon( false );
-                }
-                
-                else {
-                    $outcome = "YOU WIN";
-                    $match->setUserWon( true );
-                    $match->setCompWon( false );
-                }
-            }
-        }
-        if ( $form->get('user_scissors')->isClicked() ) {
-            $user_choice = 3;
-            $match->setUserRock( 0 );
-            $match->setUserPaper( 0 );
-            $match->setUserScissors( 1 );
-            $match->setUserLizard( 0 );
-            $match->setUserSpock( 0 );
-            if( $user_choice == $comp_choice ){
-                $outcome = "TIE";
-                $match->setCompScissors( 1 );
-                $match->setUserCompTie( true );
-                $match->setUserWon( false );
-                $match->setCompWon( false );
-            }
-            else {
-                $match->setUserCompTie( false );
-                if( $comp_choice == 5 || $comp_choice == 1 ) {
-                    $outcome = "YOU LOSE";
-                    $match->setCompWon( true );
-                    $match->setUserWon( false );
-                }
-                
-                else {
-                    $outcome = "YOU WIN";
-                    $match->setUserWon( true );
-                    $match->setCompWon( false );
-                }
-            }
-        }
-        if ( $form->get('user_lizard')->isClicked() ) {
-            $user_choice = 4;
-            $match->setUserRock( 0 );
-            $match->setUserPaper( 0 );
-            $match->setUserScissors( 0 );
-            $match->setUserLizard( 1 );
-            $match->setUserSpock( 0 );
-            if( $user_choice == $comp_choice ){
-                $outcome = "TIE";
-                $match->setCompLizard( 1 );
-                $match->setUserCompTie( true );
-                $match->setUserWon( false );
-                $match->setCompWon( false );
-            }
-            else {
-                $match->setUserCompTie( false );
-                if( $comp_choice == 3 || $comp_choice == 1 ) {
-                    $outcome = "YOU LOSE";
-                    $match->setCompWon( true );
-                    $match->setUserWon( false );
-                }
-                
-                else {
-                    $outcome = "YOU WIN";
-                    $match->setUserWon( true );
-                    $match->setCompWon( false );
-                }
-            }
-        }
-        if ( $form->get('user_spock')->isClicked() ) {
-            $user_choice = 5;
-            $match->setUserRock( 0 );
-            $match->setUserPaper( 0 );
-            $match->setUserScissors( 0 );
-            $match->setUserLizard( 0 );
-            $match->setUserSpock( 1 );
-            if( $user_choice == $comp_choice ){
-                $outcome = "TIE";
-                $match->setCompSpock( 1 );
-                $match->setUserCompTie( true );
-                $match->setUserWon( false );
-                $match->setCompWon( false );
-            }
-            else {
-                $match->setUserCompTie( false );
-                if( $comp_choice == 4 || $comp_choice == 2 ) {
-                    $outcome = "YOU LOSE";
-                    $match->setCompWon( true );
-                    $match->setUserWon( false );
-                }
-                
-                else {
-                    $outcome = "YOU WIN";
-                    $match->setUserWon( true );
-                    $match->setCompWon( false );
-                }
-            }
-        }
-
-        $match->setCreated( new \DateTime() );
-        $match->setModified( new \DateTime() );
-        $em = $this->getDoctrine()->getManager();
-        $em->persist( $match );
-        $em->flush();
+            ) {
         
 
+            $outcome = "";
+            $sessionID = $this->getRequest()->getSession()->getId();
+            $user_choice = 0;
+            $comp_choice = 0;
+            
+
+            /**
+                * 1 = rock
+                * 2 = paper
+                * 3 = scissors
+                * 4 = lizard
+                * 5 = spock
+                *
+                * SCISSORS CUTS PAPER
+                * PAPER COVERS ROCK
+                * ROCK CRUSHES LIZARD
+                * LIZARD POISONS SPOCK
+                * SPOCK SMASHES SCISSORS
+                * SCISSORS DECAPITES LIZARD
+                * LIZARD EATS PAPER
+                * PAPER DISPROVES SPOCK
+                * SPOCK VAPORIZES ROCK
+                * ROCK CRUSHES SCISSORS
+            **/
+
+            //setting computer values
+            $comp_choice = rand( 1, 5 );
+            if( $comp_choice == 1 ){
+                $match->setCompRock( 1 );
+                $match->setCompPaper( 0 );
+                $match->setCompScissors( 0 );
+                $match->setCompLizard( 0 );
+                $match->setCompSpock( 0 );
+            }
+            if( $comp_choice == 2){
+                $match->setCompRock( 0 );
+                $match->setCompPaper( 1 );
+                $match->setCompScissors( 0 );
+                $match->setCompLizard( 0 );
+                $match->setCompSpock( 0 );
+            }
+            if( $comp_choice == 3){
+                $match->setCompRock( 0 );
+                $match->setCompPaper( 0 );
+                $match->setCompScissors( 1 );
+                $match->setCompLizard( 0 );
+                $match->setCompSpock( 0 );
+            }
+            if( $comp_choice == 4){
+                $match->setCompRock( 0 );
+                $match->setCompPaper( 0 );
+                $match->setCompScissors( 0 );
+                $match->setCompLizard( 1 );
+                $match->setCompSpock( 0 );
+            }
+            if( $comp_choice == 5){
+                $match->setCompRock( 0 );
+                $match->setCompPaper( 0 );
+                $match->setCompScissors( 0 );
+                $match->setCompLizard( 0 );
+                $match->setCompSpock( 1 );
+            }
+
+            $match->setUserSid( $sessionID );
 
 
-        return $this->render('default/play.html.twig', array( 
-            'form' => $form->createView(),  'user_choice' => $user_choice, 
-            'comp_choice' => $comp_choice,   
-            'sid' => $sessionID,  
-            'outcome' => $outcome,  
-        ));
+            // game logic - who wins this "match" if user clicks ROCK?
+            if ( $form->get('user_rock')->isClicked() ) {
+                $user_choice = 1;
+                $match->setUserRock( 1 );
+                $match->setUserPaper( 0 );
+                $match->setUserScissors( 0 );
+                $match->setUserLizard( 0 );
+                $match->setUserSpock( 0 );
+                if( $user_choice == $comp_choice ){
+                    $outcome = "TIE";
+                    $match->setCompRock( 1 );
+                    $match->setUserCompTie( true );
+                    $match->setUserWon( false );
+                    $match->setCompWon( false );
+                }
+                else {
+                    $match->setUserCompTie( false );
+                    if( $comp_choice == 2 || $comp_choice == 5 ) {
+                        $outcome = "YOU LOSE";
+                        $match->setCompWon( true );
+                        $match->setUserWon( false );
+                    }
+                    
+                    else {
+                        $outcome = "YOU WIN";
+                        $match->setUserWon( true );
+                        $match->setCompWon( false );
+                    }
+                }
+                
+            }
 
+            // game logic - who wins this "match" if user clicks PAPER?
+            if ( $form->get('user_paper')->isClicked() ) {
+                $user_choice = 2;
+                $match->setUserRock( 0 );
+                $match->setUserPaper( 1 );
+                $match->setUserScissors( 0 );
+                $match->setUserLizard( 0 );
+                $match->setUserSpock( 0 );
+                if( $user_choice == $comp_choice ){
+                    $outcome = "TIE";
+                    $match->setCompPaper( 1 );
+                    $match->setUserCompTie( true );
+                    $match->setUserWon( false );
+                    $match->setCompWon( false );
+                }
+                else {
+                    $match->setUserCompTie( false );
+                    if( $comp_choice == 3 || $comp_choice == 4 ) {
+                        $outcome = "YOU LOSE";
+                        $match->setCompWon( true );
+                        $match->setUserWon( false );
+                    }
+                    
+                    else {
+                        $outcome = "YOU WIN";
+                        $match->setUserWon( true );
+                        $match->setCompWon( false );
+                    }
+                }
+            }
+
+            // game logic - who wins this "match" if user clicks SCISSORS?
+            if ( $form->get('user_scissors')->isClicked() ) {
+                $user_choice = 3;
+                $match->setUserRock( 0 );
+                $match->setUserPaper( 0 );
+                $match->setUserScissors( 1 );
+                $match->setUserLizard( 0 );
+                $match->setUserSpock( 0 );
+                if( $user_choice == $comp_choice ){
+                    $outcome = "TIE";
+                    $match->setCompScissors( 1 );
+                    $match->setUserCompTie( true );
+                    $match->setUserWon( false );
+                    $match->setCompWon( false );
+                }
+                else {
+                    $match->setUserCompTie( false );
+                    if( $comp_choice == 5 || $comp_choice == 1 ) {
+                        $outcome = "YOU LOSE";
+                        $match->setCompWon( true );
+                        $match->setUserWon( false );
+                    }
+                    
+                    else {
+                        $outcome = "YOU WIN";
+                        $match->setUserWon( true );
+                        $match->setCompWon( false );
+                    }
+                }
+            }
+
+            // game logic - who wins this "match" if user clicks LIZARD?
+            if ( $form->get('user_lizard')->isClicked() ) {
+                $user_choice = 4;
+                $match->setUserRock( 0 );
+                $match->setUserPaper( 0 );
+                $match->setUserScissors( 0 );
+                $match->setUserLizard( 1 );
+                $match->setUserSpock( 0 );
+                if( $user_choice == $comp_choice ){
+                    $outcome = "TIE";
+                    $match->setCompLizard( 1 );
+                    $match->setUserCompTie( true );
+                    $match->setUserWon( false );
+                    $match->setCompWon( false );
+                }
+                else {
+                    $match->setUserCompTie( false );
+                    if( $comp_choice == 3 || $comp_choice == 1 ) {
+                        $outcome = "YOU LOSE";
+                        $match->setCompWon( true );
+                        $match->setUserWon( false );
+                    }
+                    
+                    else {
+                        $outcome = "YOU WIN";
+                        $match->setUserWon( true );
+                        $match->setCompWon( false );
+                    }
+                }
+            }
+
+            // game logic - who wins this "match" if user clicks SPOCK?
+            if ( $form->get('user_spock')->isClicked() ) {
+                $user_choice = 5;
+                $match->setUserRock( 0 );
+                $match->setUserPaper( 0 );
+                $match->setUserScissors( 0 );
+                $match->setUserLizard( 0 );
+                $match->setUserSpock( 1 );
+                if( $user_choice == $comp_choice ){
+                    $outcome = "TIE";
+                    $match->setCompSpock( 1 );
+                    $match->setUserCompTie( true );
+                    $match->setUserWon( false );
+                    $match->setCompWon( false );
+                }
+                else {
+                    $match->setUserCompTie( false );
+                    if( $comp_choice == 4 || $comp_choice == 2 ) {
+                        $outcome = "YOU LOSE";
+                        $match->setCompWon( true );
+                        $match->setUserWon( false );
+                    }
+                    
+                    else {
+                        $outcome = "YOU WIN";
+                        $match->setUserWon( true );
+                        $match->setCompWon( false );
+                    }
+                }
+            }
+
+            //saving this "match" to database
+            $match->setCreated( new \DateTime() );
+            $match->setModified( new \DateTime() );
+            $em = $this->getDoctrine()->getManager();
+            $em->persist( $match );
+            $em->flush();
+            
+            //fetch match history
+            $wins = 0;
+            $losses = 0;
+            $repository = $this->getDoctrine()
+                ->getRepository('AppBundle:RpsslMatch');
+            $matches = $repository->findBy(
+                array('user_sid' => $sessionID ),
+                array('created' => 'DESC')
+            );
+
+            //get some stats to show score
+            $rep_match = null;
+            foreach (  $matches as $rep_match ) {
+                if( $rep_match->getUserWon() ) {
+                    $wins += 1;
+                }
+                else {
+                    $losses += 1;
+                }
+            }
+
+
+            return $this->render('default/play.html.twig', array( 
+                'form' => $form->createView(),  'user_choice' => $user_choice, 
+                'comp_choice' => $comp_choice,  
+                'outcome' => $outcome,  
+                'matches' => $matches,
+                'wins' => $wins,
+                'losses' => $losses,
+            ));
+
+        }
+
+        
     }
 
 }
