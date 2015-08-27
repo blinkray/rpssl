4f842270adadb7037a5c63e17a4b02b64fa6fa2b
Jim Schuman <jim.schuman@gmail>
2015-08-26 21:07:12 -0500

    created small test
    created small test

---
+++ src/AppBundle/Tests/Controller/PlayControllerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class PlayControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/play');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertCount(1, $crawler->filter('form'));
+        $this->assertCount(4, $crawler->filter('button'));
+    }
+
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/playAction');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertCount(1, $crawler->filter('form'));
+        $this->assertCount(4, $crawler->filter('button'));
+        $this->assertCount(3, $crawler->filter('table'));
+    }
+
+}
